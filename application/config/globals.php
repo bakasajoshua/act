3252afe20ba75de9c7ad802f61e0a8a748eb347b
Joshua Bakasa <baksajoshua09@gmail.com>
2015-11-16 18:00:17 +0300

    CodeIgniter Commit

---
+++ application/config/globals.php
@@ -0,0 +1,8 @@
+<?php
+//Default password 
+$config['default_password'] = 123456;
+
+//Key used to generate the activation key
+$config['activation'] = 'djsssssssssssssgfyewuufdshgastdhjaegy2389894dghsdhcswegribehfjhs';
+
+?>
